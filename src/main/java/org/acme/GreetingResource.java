0ed6528150af39385d50cabcd9522f0c3a22593d
gianni.franceschi <gianni.franceschifortuna@bluarancio.com>
2025-11-21 17:18:36 +0100

    saluta lucio

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello Mario from Quarkus REST";
+        return "Hello Lucio from Quarkus REST";
     }
 }
